8edaec99352e7cc3a314e860080695a1ece75f87
Heritiana <heritina.boss.etech@gmail.com>
2025-01-14 07:55:44 +0300

    Delete , post and put Agency

--- src/Controller/AgencyController.php
+++ src/Controller/AgencyController.php
@@ -8,7 +8,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
-
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 class AgencyController extends AbstractController
 {
     #[Route('/api/agencies', name: 'api_get_agencies', methods: ['GET'])]
@@ -36,5 +39,45 @@ class AgencyController extends AbstractController
         ]);
 
         return new JsonResponse($jsonAgency, 200, [], true);
+    }
+    /* api_delete_agency  using entity manager interface return jsonresponse */
+    #[Route('/api/agencies/{id}', name: 'api_delete_agency', methods: ['DELETE'])]
+    public function deleteAgency(
+        Agency $agency,
+        EntityManagerInterface $entityManager
+    ): JsonResponse
+    {
+        $entityManager->remove($agency);
+        $entityManager->flush();
+        return new JsonResponse(null, 204);
+    }
+    #[Route('/api/agencies', name: 'api_create_agency', methods: ['POST'])]
+    public function createAgency(
+        Request $request,
+        SerializerInterface $serializer,
+        EntityManagerInterface $entityManager,
+        UrlGeneratorInterface $urlGenerator
+        ): JsonResponse
+        {   
+            $agency = $serializer->deserialize($request->getContent(), Agency::class, 'json');
+            $entityManager->persist($agency);
+            $entityManager->flush();
+            $jsonAgency = $serializer->serialize($agency, 'json', ['groups' => 'agency:read']);
+            $location = $urlGenerator->generate('api_get_agency', ['id' => $agency->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+            return new JsonResponse($jsonAgency, JsonResponse::HTTP_CREATED, ["Location" => $location], true);
+        }
+        
+    #[Route('/api/agencies/{id}', name: 'api_update_agency', methods: ['PUT'])]
+    public function updateAgency(
+        Request $request,
+        SerializerInterface $serializer,
+        Agency $currentAgency,
+        EntityManagerInterface $entityManager
+    ): JsonResponse
+    {
+        $updatedAgency = $serializer->deserialize($request->getContent(), Agency::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $currentAgency]);
+        $entityManager->persist($updatedAgency);
+        $entityManager->flush();
+        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
     }
 }
